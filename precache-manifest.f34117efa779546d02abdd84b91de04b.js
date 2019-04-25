22f5039fc6e458e4344094195c3ffe311d8a96dd
MigueMartelo <miguemartelo1031@gmail.com>
2019-04-25 15:40:35 -0500

    Updates

---
+++ precache-manifest.f34117efa779546d02abdd84b91de04b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bddbde3364222d847cb235033bc29c2",
+    "url": "/calculadora3ernota/index.html"
+  },
+  {
+    "revision": "f449f7ed83eb1c1cd555",
+    "url": "/calculadora3ernota/static/css/main.6d389252.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/calculadora3ernota/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f449f7ed83eb1c1cd555",
+    "url": "/calculadora3ernota/static/js/main.94ca0ad0.chunk.js"
+  },
+  {
+    "revision": "2a48397a37e2973710a4",
+    "url": "/calculadora3ernota/static/js/runtime~main.30f1fe48.js"
+  },
+  {
+    "revision": "de6fb0dc788b624bdd4c0cf55688e35c",
+    "url": "/calculadora3ernota/static/media/logo.de6fb0dc.png"
+  }
+]);
