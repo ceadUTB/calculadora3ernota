a78cff86032e9830ff0ffa6f5b5c1ced84659fa2
miguel-q <mquintana.s2210@gmail.com>
2020-05-14 18:10:48 -0500

    boostrapjs

---
+++ build/precache-manifest.567c48f4fe8d3babc0a4e6bfa1ef03dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0d7ad507e3ece299f55c7bbf7bdf79",
+    "url": "/calculadora3ernota/index.html"
+  },
+  {
+    "revision": "1fb463aea8891019360b",
+    "url": "/calculadora3ernota/static/css/main.e4205e14.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/calculadora3ernota/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "1fb463aea8891019360b",
+    "url": "/calculadora3ernota/static/js/main.3c95b9d9.chunk.js"
+  },
+  {
+    "revision": "2a48397a37e2973710a4",
+    "url": "/calculadora3ernota/static/js/runtime~main.30f1fe48.js"
+  },
+  {
+    "revision": "de6fb0dc788b624bdd4c0cf55688e35c",
+    "url": "/calculadora3ernota/static/media/logo.de6fb0dc.png"
+  }
+]);
