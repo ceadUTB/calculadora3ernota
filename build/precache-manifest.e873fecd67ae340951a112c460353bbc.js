3eae79e91a628240ba450544e5ee3268c362fc3b
miguel-q <mquintana.s2210@gmail.com>
2020-05-14 17:58:55 -0500

    activar tabs

---
+++ build/precache-manifest.e873fecd67ae340951a112c460353bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0767d9a176cd72a4293839803c8233",
+    "url": "/calculadora3ernota/index.html"
+  },
+  {
+    "revision": "2b679a388fa9cce9171b",
+    "url": "/calculadora3ernota/static/css/main.e4205e14.chunk.css"
+  },
+  {
+    "revision": "9106383d4d3cbd8a53ab",
+    "url": "/calculadora3ernota/static/js/2.67841258.chunk.js"
+  },
+  {
+    "revision": "2b679a388fa9cce9171b",
+    "url": "/calculadora3ernota/static/js/main.c366b83b.chunk.js"
+  },
+  {
+    "revision": "2a48397a37e2973710a4",
+    "url": "/calculadora3ernota/static/js/runtime~main.30f1fe48.js"
+  },
+  {
+    "revision": "de6fb0dc788b624bdd4c0cf55688e35c",
+    "url": "/calculadora3ernota/static/media/logo.de6fb0dc.png"
+  }
+]);
