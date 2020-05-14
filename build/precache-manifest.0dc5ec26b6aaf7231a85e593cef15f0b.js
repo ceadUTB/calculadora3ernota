36003c91165dece9a8d328ed0e8c2ce5d070d19d
miguel-q <mquintana.s2210@gmail.com>
2020-05-14 16:54:36 -0500

    Se modifican los porcentajes segun la contingencia de covid19

---
+++ build/precache-manifest.0dc5ec26b6aaf7231a85e593cef15f0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979ad186fd6c1d2daf18d107129adc3d",
+    "url": "/calculadora3ernota/index.html"
+  },
+  {
+    "revision": "5ae4f95e7d7fb461b9cd",
+    "url": "/calculadora3ernota/static/css/main.e4205e14.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/calculadora3ernota/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5ae4f95e7d7fb461b9cd",
+    "url": "/calculadora3ernota/static/js/main.12105313.chunk.js"
+  },
+  {
+    "revision": "2a48397a37e2973710a4",
+    "url": "/calculadora3ernota/static/js/runtime~main.30f1fe48.js"
+  },
+  {
+    "revision": "de6fb0dc788b624bdd4c0cf55688e35c",
+    "url": "/calculadora3ernota/static/media/logo.de6fb0dc.png"
+  }
+]);
